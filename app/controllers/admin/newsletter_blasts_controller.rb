e002608f27d1ca873b1b2296c6f78948facc07af
Dave Myers <contact@ameravant.com>
2015-03-19 14:57:26 -0400

    adding delayed job

--- app/controllers/admin/newsletter_blasts_controller.rb
+++ app/controllers/admin/newsletter_blasts_controller.rb
@@ -35,9 +35,10 @@ class Admin::NewsletterBlastsController < AdminController
   end
   
   def send_blast(blast)
-    spawn do
+    #spawn do
       contacts = contacts_for_blast(blast)
-      log = Logger.new("#{RAILS_ROOT}/log/newsletter-blat-errors-#{path_safe(Time.now.to_s)}.log")
+      log = Logger.new("#{RAILS_ROOT}/log/newsletter-blast-errors-#{path_safe(Time.now.to_s)}.log")
+      log2 = Logger.new("#{RAILS_ROOT}/log/newsletter-blast-#{path_safe(Time.now.to_s)}.log")
       unsendable = 0
       contacts.each do |contact|
         if contact.active && !contact.no_newsletters
@@ -45,6 +46,7 @@ class Admin::NewsletterBlastsController < AdminController
             contact.first_name = "NotProvided" if contact.first_name.blank?
             contact.last_name = "NotProvided" if contact.last_name.blank?
             PostOffice.deliver_newsletter(blast.newsletter, contact.first_name, contact.email, contact.id, blast.id, @settings) unless contact.no_newsletters
+            log2.info "#{contact.name}, #{contact.email}, #{contact.id}:\n#{$!}"
           rescue
             log.info "The following error occurred delivery to #{contact.name}, #{contact.email}, #{contact.id}:\n#{$!}"
           end
@@ -53,11 +55,12 @@ class Admin::NewsletterBlastsController < AdminController
         end
       end
       blast.update_attributes(:recipient_count => contacts.size - unsendable)
-    end
+    #end
   end
   
   def authorization
     authorize(@permissions['newsletter_blasts'], "Newsletter Blasts")
   end
-  
+
+  handle_asynchronously :send_blast
 end
